811c8c22e8bbc88f436ff14b3fe6688af49cae4d
Ewin24 <82897868+Ewin24@users.noreply.github.com>
2023-06-26 13:22:41 -0500

    Add files via upload

---
+++ taller1/ejercicio16.py
@@ -0,0 +1,4 @@
+# Crea un programa que solicite al usuario ingresar una serie de números positivos y luego calcule e imprima el promedio
+# de los números ingresados utilizando un bucle while. El programa debe terminar cuando el usuario ingrese un número
+# negativo.
+
