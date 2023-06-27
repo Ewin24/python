185f7e2d19ecfd7bf2c56ff6949d3e287d3e5149
Ewin24 <82897868+Ewin24@users.noreply.github.com>
2023-06-27 12:49:51 -0500

    Add files via upload

--- taller1/ejercicio16.py
+++ taller1/ejercicio16.py
@@ -2,3 +2,18 @@
 # de los números ingresados utilizando un bucle while. El programa debe terminar cuando el usuario ingrese un número
 # negativo.
 
+flag = True
+lista = []
+promedio = 0
+while flag:
+    n = int(input('Ingrese un numero a la serie :'))
+    if n < 1:
+        flag = False
+    else:
+        lista.append(n)
+        
+for i in lista:
+    promedio += i
+
+promedio /= len(lista)
+print(f"El promedio de los numeros ingresados es: {promedio}")
---
+++ taller1/ejercicio20.py
@@ -0,0 +1,19 @@
+# Campus requiere administrar algunos datos de sus Campers como por ejemplo, la creación,
+# eliminación o búsqueda de los developers, entre otros, por tal razón, ha solicitado el diseño de
+# un programa que cuente con el siguiente menú como panel de control:
+# 1. CREAR GRUPO ARTEMIS
+# 1.1   LISTAR CAMPERS DE ARTEMIS
+# 1.2   AGREGAR CAMPERS DE ARTEMIS
+# 1.3   ELIMINAR CAMPERS DE ARTEMIS
+# 1.4   ORDENAR CAMPERS DE ARTEMIS
+# 1.5   BUSCAR CAMPERS DE ARTEMIS
+# 2. LISTAR CAMPERS DE SPUTNIK
+# 2.1   lISTAR CAMPERS DE SPUTNIK
+# 2.2   AGREGAR CAMPERS DE SPUTNIK
+# 2.3   ELIMINAR CAMPERS DE SPUTNIK
+# 2.4   ORDENAR CAMPERS DE SPUTNIK
+# 2.5   BUSCAR CAMPERS DE SPUTNIK
+# Digite su opcion:
+
+
+
