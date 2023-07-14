71c1d9bc51c61bed423e3c5020398823e34c0cf2
Ewin24 <82897868+Ewin24@users.noreply.github.com>
2023-07-13 22:17:01 -0500

    Add files via upload

--- retos-semana3/reto1/core.py
+++ retos-semana3/reto1/core.py
@@ -1,11 +1,33 @@
 import json
 import os
 
+'''
+-----------------------------------------------------------------
+###    Instrucciones de uso    ###
+-----------------------------------------------------------------
+filename[0:-6] : 
+
+Lo que indica es el nombre del archivo con el que se trabaja, 
+de esta forma es posible que sea generico
+
+error : 'paciente'
+Es generado cuando se busca mal la llave del json, puesto que 
+alli es llamado 'pacientes', esto por el corte de cadena ej:
+filename[0:-6] = 'paciente'
+filename[0:-5] = 'pacientes'
+filename[0:-4] = 'pacientes.'
+-----------------------------------------------------------------
+listRaza, listTipo:
+
+Funcionan unicamente con pacientes, son genericos, por lo que
+van a funcionar con cualquier modificacion den el tipoAnimal.json
+-----------------------------------------------------------------
+'''
+
 
 def create(filename, data: dict):
     if (checkFile(filename)):
         # cargamos los nuevos datos a los que ya estan
-
         fileExist = loadFile(filename)
         fileExist['pacientes'].append(data)
         # volvemos a subir los datos antiguos con actualizacion
@@ -15,7 +37,6 @@ def create(filename, data: dict):
         file.close()
     else:
         file = open('data/'+filename, 'w')
-        # ponemos el mismo nombre que se mande y quitamos .json
         jsonObj = json.dumps({filename[0:-5]: [data]}, indent=4)
         file.write(jsonObj)
         file.close()
@@ -27,21 +48,26 @@ def loadFile(filename):
     return data
 
 
-def delete(filename, id):
-    dataFile = loadFile(filename)
-    itemI = -1
-    for i, item in dataFile:
-        if item['id'] == id:
-            print(f"Busqueda exitosa en: {filename}")
-            itemI = i
-    if (itemI == -1):
-        print(f"Busqueda fallida en: {filename}")
+def delete(filename):
+    if (checkFile(filename)):
+        dataFile = loadFile(filename)
+        id = input(f"Ingrese el id del {filename[0:-6]}: ")
+        for i, item in enumerate(dataFile[filename[0:-5]]):
+            if id in (item['id']):
+                print(dataFile[filename[0:-5]][i])
+                if (bool("Confirmar la eliminacion (Enter) cancelar, (S) confirmar: ")):
+                    dataFile[filename[0:-5]].pop(i)
+                    file = open('data/'+filename, 'w')
+                    jsonObj = json.dumps({filename[0:-5]: dataFile[filename[0:-5]]}, indent=4)
+                    file.write(jsonObj)
+                    file.close()
+                else:
+                    print("El usuario cancelo la eliminacion")
+            else:
+                print(f"Busqueda fallida en: {filename[0:-6]}")
     else:
-        if (bool("Confirmar la eliminacion (Enter) cancelar, (S) confirmar: " + dataFile[itemI]) == False):
-            dataFile[itemI].pop(itemI)
-            create(filename, dataFile)
-        else:
-            print("El usuario cancelo la eliminacion")
+        print("No se tienen registros aun, no es posible hacer una eliminacion")
+    # TODO: HAY QUE TERMINAR LA FUNCION, HAY UN ERROR EN COMO SE ELIMINA EL OBJ EN EL JSON (solved)
 
 
 def update():
@@ -51,6 +77,7 @@ def update():
 def listAll(filename):
     dataFile = loadFile(filename)
     print(dataFile)
+    # TODO: HACER QUE CADA REGISTRO SE DIVIDA DE MANERA CORRECTA Y QUEDE ORDENADO EN UNA TABLA
 
 
 def listById():
@@ -85,15 +112,18 @@ def checkFile(fileName):
         return False
 
 
-def checkId(filename, id):
-    bandera = True
-    file = loadFile(filename)
-    for i, item in enumerate(file):
-        if id in (item['id']):
-            print(f"El id {id} ya se encuentra registrado")
-            return False
-        else:
-            for i, item in enumerate(file):
-                if id in (item['id']):
-                    print(f"El id {id} ya se encuentra registrado")
-                    return False
+def checkId(filename):
+    bandera = ''
+    if (checkFile(filename)):
+        file = loadFile(filename)
+        id = input(f"Ingrese el id del {filename[0:-6]}: ")
+        for i, item in enumerate(file[filename[0:-5]]):
+            if id in (item['id']):
+                bandera = False
+            else:
+                bandera = id
+        return bandera
+    else:
+        id = input(f"Ingrese el id del {filename[0:-6]}: ")
+        bandera = id
+        return bandera
--- retos-semana3/reto1/main.py
+++ retos-semana3/reto1/main.py
@@ -4,23 +4,23 @@ import cita
 import veterinario
 
 if __name__ == "__main__":
-    import os
-
-    flag = True
-    while (flag):
-        os.system("clear")
-        print("----ADMINISTRACION DEL CENTRO VETERINARIO----")
-        print("1. Gestion de pacientes")
-        print("2. Gestion de veterinarios")
-        print("3. Gestion de citas medicas")
-        print("4. Salir")
-        opc = int(input(":)_ "))
-
-        if opc == 1:
-            paciente.menu()
-        if opc == 2:
-            veterinario.menu()
-        if opc == 3:
-            cita.menu()
-        if opc == 4:
-            flag = False
+    try:
+        flag = True
+        while (flag):
+            os.system("clear")
+            print("----ADMINISTRACION DEL CENTRO VETERINARIO----")
+            print("1. Gestion de pacientes")
+            print("2. Gestion de veterinarios")
+            print("3. Gestion de citas medicas")
+            print("4. Salir")
+            opc = int(input(":)_ "))
+            if opc == 1:
+                paciente.menu()
+            if opc == 2:
+                veterinario.menu()
+            if opc == 3:
+                cita.menu()
+            if opc == 4:
+                flag = False
+    except Exception as e:
+        print(e)
